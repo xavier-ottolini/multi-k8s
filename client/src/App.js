f09afb914c79d75f7c5279b4685fccdd1b6d9d44
xavier-ottolini <xavophonic@yahoo.fr>
2024-05-22 11:30:42 +0200

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Apprendre React
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
